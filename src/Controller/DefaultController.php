01919c89c019396f4c78a8ea627ccb3dc1ae1104
julien-35 <julien45.dubois@gmail.com>
2024-07-21 22:27:17 +0200

    Test url dans defaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,27 +7,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function index(): Response
-    {
-        return new Response('Hello world !!');
-    }
-
-    /**
-     * @Route("/api/avis/get", name="avis_get", methods={"GET"})
-     */
-    public function getAvis(): Response
-    {
-        return new Response('get avis');
-    }
-
-    /**
-     * @Route("/api/horaire/get", name="horaire_get", methods={"GET"})
-     */
-    public function getHoraire(): Response
-    {
-        return $this->json(['message' => 'get horaire']);
-    }
+    // /**
+    //  * @Route("/", name="homepage")
+    //  */
+    // public function index(): Response
+    // {
+    //     return new Response('Hello world !!');
+    // }
 }
