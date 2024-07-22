92a179aa7a71cec4848fca5825e813b6a2e0100b
julien-35 <julien45.dubois@gmail.com>
2024-07-22 10:14:51 +0200

    Ajout index dans defaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,11 +7,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    // /**
-    //  * @Route("/", name="homepage")
-    //  */
-    // public function index(): Response
-    // {
-    //     return new Response('Hello world !!');
-    // }
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return new Response('Hello world !!');
+    }
 }
